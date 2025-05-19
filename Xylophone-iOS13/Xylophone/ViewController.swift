53a8f27fe78a400f38d1124e0340583a38572c77
Daffa Putera K <kouseinadaffa@gmail.com>
2025-05-19 17:57:18 +0700

    feat(xylophone): tap

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-    
     @IBAction func keyPressed(_ sender: UIButton) {
-        
+        playSound(soundName: sender.titleLabel?.text ?? "")
     }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
